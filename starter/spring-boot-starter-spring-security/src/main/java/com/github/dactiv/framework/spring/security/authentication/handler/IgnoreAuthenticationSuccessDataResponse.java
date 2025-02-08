f90a8b3c455a54c41813454823c4c71338d72ce3
chenxiaobo <chan.alex@foxmail.com>
2025-02-08 17:30:02 +0800

    删除一下多余的代码。

--- starter/spring-boot-starter-spring-security/src/main/java/com/github/dactiv/framework/spring/security/authentication/handler/IgnoreAuthenticationSuccessDataResponse.java
+++ starter/spring-boot-starter-spring-security/src/main/java/com/github/dactiv/framework/spring/security/authentication/handler/IgnoreAuthenticationSuccessDataResponse.java
@@ -14,7 +14,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * 认证成功后的忽略相应字段处理
@@ -40,15 +39,7 @@ public class IgnoreAuthenticationSuccessDataResponse implements JsonAuthenticati
 
         Object details = result.getData();
         DocumentContext documentContext = createDocumentContext(details);
-
-        for (String property : properties) {
-            Object value = documentContext.read(property);
-            if (Objects.isNull(value)) {
-                continue;
-            }
-            documentContext.delete(property);
-        }
-
+        properties.forEach(documentContext::delete);
         result.setData(documentContext.json());
     }
 
